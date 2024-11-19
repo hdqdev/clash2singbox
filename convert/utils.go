be987e60d9b54c89c4110704d7213ef0a22497ef
hdqdev <hdqdev@gmail.com>
2024-11-19 18:26:42 +0800

    优化节点选择

--- convert/utils.go
+++ convert/utils.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/xmdhs/clash2singbox/model/clash"
 	"github.com/xmdhs/clash2singbox/model/singbox"
@@ -62,14 +63,14 @@ func getTags(s []singbox.SingBoxOut) []string {
 	})
 }
 
-func Patch(b []byte, s []singbox.SingBoxOut, include, exclude string, extOut []interface{}, extags ...string) ([]byte, error) {
-	d, err := PatchMap(b, s, include, exclude, extOut, extags, true, true)
+func Patch(b []byte, s []singbox.SingBoxOut, urltestOut bool, include, exclude string, extOut []interface{}, extags ...string) ([]byte, error) {
+	d, err := PatchMap(b, s, include, exclude, extOut, extags, urltestOut, true)
 	if err != nil {
 		return nil, fmt.Errorf("Patch: %w", err)
 	}
 	bw := &bytes.Buffer{}
 	jw := json.NewEncoder(bw)
-	jw.SetIndent("", "    ")
+	jw.SetIndent("", "  ")
 	err = jw.Encode(d)
 	if err != nil {
 		return nil, fmt.Errorf("Patch: %w", err)
@@ -83,6 +84,15 @@ func ToInsecure(c *clash.Clash) {
 		p.SkipCertVerify = true
 		c.Proxies[i] = p
 	}
+}
+
+func contains(slice []string, item string) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
 }
 
 func PatchMap(
@@ -133,9 +143,43 @@ func PatchMap(
 		})
 	}
 
+	confTemplate, err := parseTemplate(tpl)
+	if err != nil {
+		return nil, fmt.Errorf("PatchMap parseTemplate faild: %w", err)
+	}
+
+	fileredOutbounds, emptyOutbounds, err := filerOutbounds(confTemplate, ftags)
+	if err != nil {
+		return nil, fmt.Errorf("PatchMap filter outbounds faild: %w", err)
+	}
+
+	if len(fileredOutbounds) > 0 {
+		anyList = append(anyList, fileredOutbounds...)
+	}
+
 	anyList = append(anyList, extOut...)
 	for _, v := range s {
 		anyList = append(anyList, v)
+	}
+
+	for i := 0; i < len(anyList); i++ {
+		outbound, ok := anyList[i].(singbox.SingBoxOut)
+		if !ok || outbound.Outbounds == nil {
+			continue
+		}
+		var outbounds []string
+		for _, tag := range outbound.Outbounds {
+			if !contains(emptyOutbounds, tag) {
+				outbounds = append(outbounds, tag)
+			}
+		}
+		if len(outbounds) == 0 {
+			anyList = append(anyList[:i], anyList[i+1:]...)
+			i--
+		} else {
+			outbound.Outbounds = outbounds
+			anyList[i] = outbound
+		}
 	}
 
 	anyList = append(anyList, singbox.SingBoxOut{
@@ -157,4 +201,101 @@ func PatchMap(
 	d["outbounds"] = anyList
 
 	return d, nil
+}
+
+var predefinedOutbounds = []singbox.SingBoxOut{
+	{Type: "direct", Tag: "direct"},
+	{Type: "block", Tag: "block"},
+	{Type: "dns", Tag: "dns-out"},
+}
+
+func isPredefinedOutbound(out singbox.SingBoxOut) bool {
+	for _, predefined := range predefinedOutbounds {
+		if out.Type == predefined.Type && out.Tag == predefined.Tag {
+			return true
+		}
+	}
+	return false
+}
+
+func parseTemplate(tpl []byte) (singbox.TemplatePartialConfig, error) {
+	var partialConfig singbox.TemplatePartialConfig
+	err := json.Unmarshal(tpl, &partialConfig)
+	if err != nil {
+		return singbox.TemplatePartialConfig{}, fmt.Errorf("parseTemplate: %w", err)
+	}
+	return partialConfig, nil
+}
+
+func filerOutbounds(partialConfig singbox.TemplatePartialConfig, ftags []string) ([]any, []string, error) {
+	var filteredOutbounds []any
+	var emptyOutbounds []string
+	if len(partialConfig.Outbounds) > 0 {
+		var outboundsTemp []singbox.SingBoxOut
+		err := json.Unmarshal(partialConfig.Outbounds, &outboundsTemp)
+		if err != nil {
+			fmt.Println("Error parsing outbounds:", err)
+		}
+		custemNodesTypes := []string{"vmess", "shadowsocks", "trojan", "snell", "socks5", "http", "https", "ssr", "vless", "wireguard"}
+		for _, outbound := range outboundsTemp {
+			if contains(custemNodesTypes, outbound.Type) {
+				ftags = append(ftags, outbound.Tag)
+			}
+		}
+
+		for _, outbound := range outboundsTemp {
+			if outbound.Filter != nil {
+				for _, filterInfo := range *outbound.Filter {
+					var filtered []string
+					if filterInfo.Action == "include" {
+						if filterInfo.Regexp != "" {
+							regexp := filterInfo.Regexp
+							if regexp == "{all}" {
+								regexp = ".*"
+							}
+							filtered_grep, err := filter(true, regexp, ftags)
+							if err != nil {
+								return nil, nil, fmt.Errorf("filter Tag %s regexp [%s] faild: %w", outbound.Tag, filterInfo.Regexp, err)
+							}
+							if len(ftags) > 0 {
+								filtered = append(filtered, filtered_grep...)
+							}
+						} else if len(filterInfo.Keywords) > 0 {
+							regexp := strings.Join(filterInfo.Keywords, "|")
+							filtered_grep, err := filter(true, regexp, ftags)
+							if err != nil {
+								return nil, nil, err
+							}
+							if len(filtered_grep) > 0 {
+								filtered = append(filtered, filtered_grep...)
+							}
+						}
+					} else if filterInfo.Action == "exclude" {
+						regexp := strings.Join(filterInfo.Keywords, "|")
+						filtered_grep, err := filter(false, regexp, ftags)
+						if err != nil {
+							return nil, nil, err
+						}
+						if len(filtered_grep) > 0 {
+							filtered = append(filtered, filtered_grep...)
+						}
+					}
+					if len(filtered) > 0 {
+						outbound.Outbounds = append(outbound.Outbounds, filtered...)
+						outbound.Filter = nil
+						filteredOutbounds = append(filteredOutbounds, outbound)
+					} else {
+						fmt.Printf("Tag %s canot filer nodes keywords [%s] regexp [%s]\n", outbound.Tag, strings.Join(filterInfo.Keywords, "|"), filterInfo.Regexp)
+						emptyOutbounds = append(emptyOutbounds, outbound.Tag)
+					}
+				}
+			} else {
+				if isPredefinedOutbound(outbound) {
+					continue
+				}
+				filteredOutbounds = append(filteredOutbounds, outbound)
+			}
+		}
+	}
+	return filteredOutbounds, emptyOutbounds, nil
 }
